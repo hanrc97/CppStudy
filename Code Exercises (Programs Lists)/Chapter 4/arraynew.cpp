dac8ca25c7ef10c828a95d4b68277b19f716bb81
hanrc97 <hanrc1997@gmail.com>
2020-08-11 16:53:47 +0800

    update

---
+++ Code Exercises (Programs Lists)/Chapter 4/arraynew.cpp
@@ -0,0 +1,30 @@
+// arraynew.cpp -- using the new operator for arrays
+#include <iostream>
+int main()
+{
+	using namespace std;
+	double * p3 = new double [3]; // allocate space for 3 doubles
+	p3[0] = 0.2;									// treat p3 like an array name
+	p3[1] = 0.5;
+	p3[2] = 0.8;
+	cout << "p3[1] is " << p3[1] << ".\n";
+	p3 = p3 + 1;                  // increment the pointer, it points to 0.5
+	cout << "Now p3[0] is " << p3[0] << " and ";
+	cout << "p3[1] is " << p3[1] << ".\n";
+	p3 = p3 - 1;									// point back to beginning
+	delete [] p3;									// free the memory
+	cin.get();
+	p3 = new double [4];          // then allocate a new space for 4 doubles
+	p3[0] = 2.0;
+	p3[1] = 5.0;
+	p3[2] = 8.0;
+	p3[3] = 10.0;
+	cout << "p3[1] is " << p3[1] << ".\n";
+	p3 = p3 + 1;                  // increment the pointer, it points to 5.0
+	cout << "Now p3[0] is " << p3[0] << " and ";
+	cout << "p3[1] is " << p3[1] << ".\n";
+	p3 = p3 - 1;									// point back to beginning
+	delete [] p3;									// free the memory
+	cin.get();
+	return 0; 
+}
